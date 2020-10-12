e9d3fa49b2b1ce8d7fa86af95be0bdabd2c26ae7
ManuelArto <manuelartociao@gmail.com>
2020-10-12 12:35:52 +0200

    Edit TUI/*

--- Selenium/TUI/SpotifyMp3.py
+++ Selenium/TUI/SpotifyMp3.py
@@ -3,82 +3,101 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-import youtube_dl
 import os
 
 
 class SpotifyMp3:
+	def __init__(self, url, path):
+		options = Options()
+		options.headless = True
+		prefs = {"profile.managed_default_content_settings.images": 2}
+		options.add_experimental_option("prefs", prefs)
+		self.driver = webdriver.Chrome(
+			options=options, executable_path="/home/manuel/Downloads/chromedriver"
+		)
+		self.path = path
+		self.url = url
 
-    def __init__(self, url, path):
-        options = Options()
-        options.headless = True
-        prefs = {'profile.managed_default_content_settings.images': 2}
-        options.add_experimental_option("prefs", prefs)
-        self.driver = webdriver.Chrome(options=options, executable_path="/home/manuel/Downloads/chromedriver")
-        self.path = path
-        self.url = url
+	def closeBrowser(self):
+		self.driver.quit()
 
-    def closeBrowser(self):
-        self.driver.quit()
+	def start(self):
+		self.get_titles()
+		self.get_links()
+		self.download_from_yt()
 
-    def start(self):
-        self.get_titles()
-        self.get_links()
-        self.download_from_yt()
+	def get_titles(self):
+		print("getting titles")
+		driver = self.driver
+		driver.get(self.url)
+		titles = WebDriverWait(driver, 5).until(
+			EC.visibility_of_all_elements_located((By.CLASS_NAME, "tracklist-name"))
+		)
+		artists = driver.find_elements_by_class_name("TrackListRow__artists")
+		self.path += str(driver.title) if self.path != "" else "" + "/"
+		file = open("./files/songs.txt", "w+")
+		for song, artist in zip(titles, artists):
+			file.write(song.text + " " + artist.text + "\n")
+		file.close()
 
-    def get_titles(self):
-        print("getting titles")
-        driver = self.driver
-        driver.get(self.url)
-        titles = WebDriverWait(driver, 5).until(
-                     EC.visibility_of_all_elements_located((By.CLASS_NAME, "tracklist-name")))
-        artists = driver.find_elements_by_class_name("TrackListRow__artists")
-        self.path += str(driver.title) + "/"
-        file = open("./files/songs.txt", 'w+')
-        for song, artist in zip(titles, artists):
-            file.write(song.text + " " + artist.text + "\n")
-        file.close()
+	def get_links(self):
+		print("getting links")
+		driver = self.driver
+		file = open("./files/songs.txt", "r")
+		songs = file.readlines()
+		file = open("./files/links.txt", "w+")
+		for song in songs:
+			try:
+				driver.get("http://www.youtube.com/results?search_query=" + song)
+				link = (
+					WebDriverWait(driver, 5)
+					.until(
+						EC.visibility_of_all_elements_located((By.ID, "video-title"))
+					)[0]
+					.get_attribute("href")
+				)
+				file.write(link + "\n")
+			except Exception as e:
+				print(
+					song + " at number " + str(songs.index(song)) + " not found. ERROR:"
+				)
+				print(e)
+		file.close()
+		self.closeBrowser()
 
-    def get_links(self):
-        print("getting links")
-        driver = self.driver
-        file = open("./files/songs.txt", 'r')
-        songs = file.readlines()
-        file = open("./files/links.txt", 'w+')
-        for song in songs:
-            try:
-                driver.get("http://www.youtube.com/results?search_query=" + song)
-                link = WebDriverWait(driver, 5).until(
-                       EC.visibility_of_all_elements_located((By.ID, "video-title")))[0].get_attribute("href")
-                file.write(link + "\n")
-            except Exception as e:
-                print(song + " at number " + str(songs.index(song)) + " not found. ERROR:")
-                print(e)
-        file.close()
-        self.closeBrowser()
+	def download_from_yt(self):
+		try:
+			self.closeBrowser()
+			print("downloading")
+			file = open("./files/links.txt", "r")
+			links = file.readlines()
+			try:
+				os.mkdir(self.path)
+			except Exception as e:
+				print(e)
+			os.chdir(self.path)
 
-    def download_from_yt(self):
-        try:
-            self.closeBrowser()
-            print("downloading")
-            download_options = {
-                'format': 'bestaudio/best',
-                'outtmpl': '%(title)s.mp3',
-                'nocheckcertificate': True,
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192',
-                }],
-            }
-            file = open("./files/links.txt", 'r')
-            links = file.readlines()
-            os.mkdir(self.path)
-            os.chdir(self.path)
-            with youtube_dl.YoutubeDL(download_options) as dl:
-                dl.download(links)
-        except Exception as e:
-            print(e)
+			# youtube-dl cli
+			for link in links:
+				os.system(f"youtube-dl -x --audio-format mp3 {link}")
 
 
-# for manual testing "python SpotifyMp3.py -i"
+			# youtube_dl library
+			# import youtube_dl
+			# download_options = {
+			# 	"format": "bestaudio/best",
+			# 	"postprocessors": [
+			# 		{
+			# 			"key": "FFmpegExtractAudio",
+			# 			"preferredcodec": "m4a",
+			# 			"preferredquality": "192",
+			# 		}
+			# 	],
+			# 	"postprocessor_args": ["-ar", "16000"],
+			# 	"prefer_ffmpeg": True,
+			# 	"keepvideo": True,
+			# }
+			# with youtube_dl.YoutubeDL(download_options) as dl:
+			# 	dl.download(links)
+		except Exception as e:
+			print(e)
--- Selenium/TUI/main.py
+++ Selenium/TUI/main.py
@@ -2,10 +2,21 @@ from SpotifyMp3 import SpotifyMp3
 
 spotifyMp3 = ()
 try:
-    url = input("Inserisci l'URL della playlist di spotify\n")
-    # path = input("Inserisci il nome della cartella di download\n")
-    spotifyMp3 = SpotifyMp3(url, "")
-    print("Started")
-    spotifyMp3.start()
+	url = input("Inserisci l'URL della playlist di spotify: ")
+	path = input("Inserisci il nome della cartella di download (leave blank for spotify playlist title): ")
+	spotifyMp3 = SpotifyMp3(url, path)
+	print("[STARTING]")
+	while True:
+		choice = input("1. Save songs' title\n2. Save songs' link\n3. Download songs\n4. Break\nSelect: ")
+		if choice == "1":
+			spotifyMp3.get_titles()
+		elif choice == "2":
+			spotifyMp3.get_links()
+		elif choice == "3":
+			spotifyMp3.download_from_yt()
+		elif choice == "4":
+			break
+		else:
+			print("Select a valide choice")
 except Exception as e:
-    print(e)
+	print(e)
